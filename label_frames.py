3e976b74e9de9048b877bf446b5b09d56173f328
iranroman <iran@ccrma.stanford.edu>
2022-06-22 13:11:08 -0400

    sysarg to do a participant at a time

--- label_frames.py
+++ label_frames.py
@@ -1,7 +1,10 @@
 import csv
 import os
+import sys
 from operator import itemgetter
 from pathlib import Path
+
+PNN = sys.argv[1]
 
 filename='epic-kitchens-100-annotations/EPIC_100_train.csv'
 frames_path = '/vast/irr2020/EPIC-KITCHENS/frames'
@@ -12,9 +15,6 @@ with open(filename, newline='') as csvfile:
         ann_lines.append(row)
 
 ann_lines = ann_lines[1:]
-
-# sort by participants
-sorted(ann_lines, key=itemgetter(1))
 
 # split participants
 PNN_dict={}
@@ -28,29 +28,27 @@ for line in ann_lines:
     if video_id not in PNN_dict[participant_id]: PNN_dict[participant_id][video_id] = []
     PNN_dict[participant_id][video_id].append([narration, start_frame, stop_frame])
 
-for participant_id, PNN_NN_dict in PNN_dict.items():
-    for video_id, act_stamps in PNN_NN_dict.items():
-        # see if video frames exist
-        try:
-            if not os.path.isdir(os.path.join(frames_path,participant_id,video_id)):
-                raise ValueError('participant {} video {} not found'.format(participant_id,video_id))
-        except ValueError:
-            continue
+#for participant_id, PNN_NN_dict in PNN_dict.items():
+for video_id, act_stamps in PNN_dict[PNN].items():
+    # see if video frames exist
+    try:
+        if not os.path.isdir(os.path.join(frames_path,participant_id,video_id)):
+            raise ValueError('participant {} video {} not found'.format(participant_id,video_id))
+    except ValueError:
+        continue
 
-        # get labels path and create it if needed
-        labels_path = os.path.join(frames_path[:-6],'labels',participant_id,video_id)
-        if not os.path.isdir(labels_path):
-            os.makedirs(labels_path)
+    # get labels path and create it if needed
+    labels_path = os.path.join(frames_path[:-6],'labels',participant_id,video_id)
+    if not os.path.isdir(labels_path):
+        os.makedirs(labels_path)
 
-        for act_stamp in act_stamps:
-            action, start_frame, stop_frame = act_stamp
-            start_frame = start_frame + 1 if start_frame % 2 == 1 else start_frame
+    for act_stamp in act_stamps:
+        action, start_frame, stop_frame = act_stamp
+        start_frame = start_frame + 1 if start_frame % 2 == 1 else start_frame
 
-            for frame in range(start_frame,stop_frame,2):
-                assert os.path.isfile(os.path.join(frames_path, participant_id, video_id,'{:07}.jpg'.format(frame)))
-                frame_file = os.path.join(labels_path,'{:07d}.txt'.format(frame))
-                if not os.path.isfile(frame_file):
-                    Path(frame_file).touch()
+        for frame in range(start_frame,stop_frame,2):
+            assert os.path.isfile(os.path.join(frames_path, participant_id, video_id,'{:07}.jpg'.format(frame)))
+            frame_file = os.path.join(labels_path,'{:07d}.txt'.format(frame))
 
-                with open(frame_file, 'a') as f:
-                    f.write(action + '\n')
+            with open(frame_file, 'a+') as f:
+                f.write(action + '\n')
